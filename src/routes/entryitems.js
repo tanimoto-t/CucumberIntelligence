67fabca165bb89b1428c858675ae97feaa3b52b6
谷本達弥 <gervard4869@gmail.comm>
2020-01-28 18:21:22 +0900

    add db connection sample source

--- src/routes/entryitems.js
+++ src/routes/entryitems.js
@@ -4,6 +4,40 @@
 
 "use strict";
 
+var itemName;
+var itemDescription;
+var category;
+var condition;
+var startDate;
+var endDate;
+
+var { Client } = require('pg');
+
+var client = new Client({
+    user: 'cucumber',
+    host: 'localhost',
+    database: 'CucumberIntelligence',
+    password: 'Password12',
+    port: 5432
+})
+
+client.connect()
+
+client.query('SELECT CURRENT_TIMESTAMP',
+  (err, res) => {
+    console.log(res);
+    console.log(err);
+    // client.end();
+  })
+
+client.query('SELECT * FROM items',
+  (err, res) => {
+    console.log(res);
+    console.log(err);
+    client.end();
+  })
+
+
 class Index {
     constructor() {}
 
